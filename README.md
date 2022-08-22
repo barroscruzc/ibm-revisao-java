5431708a8f0f89305fb9b0b3108668f459eb8910
barroscruzc <barroscruzc@gmail.com>
2022-08-22 11:31:27 -0300

    iniciando projeto com spring no console

---
+++ README.md
@@ -0,0 +1,5 @@
+```shell
+mvn spring-boot: start #rodando com o maven instalado no computador #rodando com o maven instalado no computador
+./mvnw spring-boot:start #roda com unix e o binário do maven na pasta oculta .mvn -> gitbash e subsystem (ubuntu dentro do windows)
+mvnw.cmd spring-boot:start # roda com MSDos(windows) e o binário do maven na pasta oculta .mvn.
+```
---
+++ src/main/java/com/br/profdanilo/console/LogicaApplication.java
@@ -0,0 +1,14 @@
+package com.br.profdanilo.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Bem vindo ao app java console");
+		SpringApplication.run(LogicaApplication.class, args);
+	}
+
+}
